4d06c216c259eb97acf32ffd6bd532de8da60b23
filipe.vilanova <filipe.vilanova@armazemparaiba.com>
2024-07-11 17:55:05 -0300

    integrando com o banco e aprendendo pragma: aula 5

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok(client):
     response = client.get('/')  # Act (ação)
@@ -30,18 +32,18 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'testusername',
-                'email': 'test@test.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -73,12 +75,12 @@ def test_update_user_nonexistent(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_read_user_by_id(client):
+def test_read_user_by_id(client, user):
     response = client.get('/users/1')
 
     assert response.json() == {
-        'username': 'testusername2',
-        'email': 'test@test.com',
+        'username': 'Teste',
+        'email': 'teste@test.com',
         'id': 1,
     }
 
@@ -89,7 +91,7 @@ def test_read_user_by_id_nonexistent(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     print(response)
